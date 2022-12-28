d44cf4b76edb90cbc398107a1f3fd35871978526
marshtv <marshtv.1972@gmail.com>
2022-12-28 07:29:02 +0300

    Complete_Alpha

--- main.cpp
+++ main.cpp
@@ -3,21 +3,28 @@
 #include <string>
 
 bool bAnagram(std::string& str_1, std::string& str_2) {
-	std::map<int, char> oStr_1, oStr_2;
+	std::map<char, int> oStr_1, oStr_2;
+	std::pair<char, int> oPair_1, oPair_2;
 	if (str_1.length() != str_2.length()) {
 		return false;
 	} else {
 		for (int i = 0; i < str_1.length(); i++) {
-			std::pair<int, char> oPair_1;
-			oPair_1.first = i;
-			oPair_1.second = str_1[i];
+			oPair_1.first = str_1[i];
+			oPair_1.second = i;
 			oStr_1.insert(oPair_1);
-			std::pair<int, char> oPair_2;
-			oPair_2.first = i;
-			oPair_2.second = str_2[i];
+			oPair_2.first = str_2[i];
+			oPair_2.second = i;
 			oStr_2.insert(oPair_2);
 		}
+		std::map<char, int>::iterator it_2 = oStr_2.begin();
+		for (std::map<char, int>::iterator it_1 = oStr_1.begin(); it_1 != oStr_1.end(); it_1++) {
+			if (it_1->first != it_2->first)
+				return false;
+			else
+				it_2++;
+		}
 	}
+	return true;
 }
 
 int main() {
@@ -25,5 +32,9 @@ int main() {
 
 	std::cout << "Input two strings to check for anagrams:" << std::endl;
 	std::cin >> str_1 >> str_2;
-	bAnagram(str_1, str_2);
+
+	if (bAnagram(str_1, str_2))
+		std::cout << "This strings are anagrams!" << std::endl;
+	else
+		std::cout << "This strings are not anagrams!" << std::endl;
 }
